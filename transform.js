09dcab3e74a5eecf3ce59dbd76c1f634787d07d7
ScottZ126 <zskang0126@gmail.com>
2024-10-14 20:28:58 -0400

    Add files via upload

---
+++ transform.js
@@ -0,0 +1,21 @@
+const heroImage = document.querySelector('.hero-image')
+
+const background = ["background-image: url('images/cat-hero.jpeg');",
+                    "background-image: url('images/b2.jpg');",
+                    "background-image: url('images/b3.jpg');",
+                    "background-image: url('images/b4.jpg');",
+                    "background-image: url('images/b5.jpg');",
+                    "background-image: url('images/b6.jpg');",]
+let index = 1
+
+function updateImage() {
+    heroImage.style = background[index]
+
+    index++
+
+    if(index > background.length-1) {
+        index = 0
+    }
+}
+
+setInterval(updateImage, 10000)
