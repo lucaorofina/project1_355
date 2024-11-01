7e01d8e0d71124661ed92f09dac4e7d85d467766
scottz <zskang0126@gmail.com>
2024-11-01 00:01:33 -0400

    Update transform.js

--- transform.js
+++ transform.js
@@ -1,11 +1,11 @@
 const heroImage = document.querySelector('.hero-image')
 
-const background = ["background-image: url('images/b1.jpg');",
-    "background-image: url('images/b2.jpg');",
-    "background-image: url('images/b3.jpg');",
-    "background-image: url('images/b4.jpg');",
-    "background-image: url('images/b5.jpg');",
-    "background-image: url('images/b6.jpg');",]
+const background = ["background-image: url('home/b1.jpg');",
+                    "background-image: url('home/b2.jpg');",
+                    "background-image: url('home/b3.jpg');",
+                    "background-image: url('home/b4.jpg');",
+                    "background-image: url('home/b5.jpg');",
+                    "background-image: url('home/b6.jpg');",]
 let index = 1
 
 function updateImage() {
